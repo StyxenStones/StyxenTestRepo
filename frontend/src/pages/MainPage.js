3e16c2357c74cf01c2faeeb8a4e5a66ed24f8f4f
Joseph Manalo <josephmanalo@knights.ucf.edu>
2020-11-03 20:34:11 -0500

    Changed Name to Header

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Main from '../components/Main';
-import LoggedInName from '../components/LoggedInName';
+import LoggedInName from '../components/Header';
 
 const MainPage = () =>
 {
