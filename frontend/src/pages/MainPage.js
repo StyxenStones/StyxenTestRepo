a05ab3db3a9781c012dc0971c64ce4fa6d70c2cd
Joseph Manalo <josephmanalo@knights.ucf.edu>
2020-11-03 20:50:05 -0500

    Import fix

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Main from '../components/Main';
-import LoggedInName from '../components/Header';
+import Header from '../components/Header';
 
 const MainPage = () =>
 {
