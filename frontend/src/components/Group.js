c51316546553a165d425e20a02aeba42b361ef56
StyxenStones <joseph70590@gmailcom>
2020-11-22 02:45:26 -0500

    spacing

--- frontend/src/components/Group.js
+++ frontend/src/components/Group.js
@@ -71,43 +71,43 @@ function Group() {
                 </div>
                 <button type="button" id="addMemberButton" class="buttons"> Add Member </button>
             </div>
-            <div id='groupMoviesDiv' >
+            <div id="groupMoviesDiv">
                 <GroupMoviesList />
             </div>
             <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#form">
                 Edit Group
             </button>
-                <div>
+            <div>
                 <div class="modal fade" id="form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                    <div class="modal-header border-bottom-0">
-                        <h5 class="modal-title" id="exampleModalLabel">Edit Group</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header border-bottom-0">
+                                <h5 class="modal-title" id="exampleModalLabel">Edit Group</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div>
+                                <form onSubmit={doEditGroup}>
+                                    <div class="modal-body">
+                                        <div class="form-group">
+                                            <label for="groupname">Group Name</label>
+                                            <input type="text" class="form-control" id="group-name" aria-describedby="group-name" placeholder={groupName} ref={(c) => editGroupName = c}></input>
+                                            <small id="group-name" class="form-text text-muted">Name your group</small>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="group-description">Group Description</label>
+                                            <input type="text" class="form-control" id="group-description" aria-describedby="group-description" placeholder={groupDescription} ref={(c) => editGroupDescription = c}></input>
+                                            <small id="group-description" class="form-text text-muted">Describe your group</small>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer border-top-0 d-flex justify-content-center">
+                                        <button type="submit" class="btn btn-success" onClick={doEditGroup}>Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                    <form onSubmit={doEditGroup}>
-                        <div class="modal-body">
-                            <div class="form-group">
-                                <label for="groupname">Group Name</label>
-                                <input type="text" class="form-control" id="group-name" aria-describedby="group-name" placeholder={groupName} ref={(c) => editGroupName = c}></input>
-                                <small id="group-name" class="form-text text-muted">Name your group</small>
-                            </div>
-                            <div class="form-group">
-                                <label for="group-description">Group Description</label>
-                                <input type="text" class="form-control" id="group-description" aria-describedby="group-description" placeholder={groupDescription} ref={(c) => editGroupDescription = c}></input>
-                                <small id="group-description" class="form-text text-muted">Describe your group</small>
-                            </div>
-                        </div>
-                        <div class="modal-footer border-top-0 d-flex justify-content-center">
-                        <button type="submit" class="btn btn-success" onClick={doEditGroup}>Submit</button>
-                        </div>
-                    </form>
-                </div>
-                </div>
-                </div>
                 </div>
             </div>
         </div>
