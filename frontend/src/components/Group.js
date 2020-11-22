31ec6a057b3207c9de684bbd01d338d4f85afbee
StyxenStones <joseph70590@gmailcom>
2020-11-22 02:46:21 -0500

    helmet?

--- frontend/src/components/Group.js
+++ frontend/src/components/Group.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import GroupMoviesList from './GroupMoviesList'
+import {Helmet} from "react-helmet";
 
 const app_name = 'cine-mates'
 function buildPath(route) {
@@ -64,6 +65,11 @@ function Group() {
 
     return(
         <div>
+            <Helmet>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+            </Helmet>
             <div id="groupInfo">
                 <h1 id="groupName">{groupName}</h1>
                 <h3 id="groupDescription">{groupDescription}</h3>
