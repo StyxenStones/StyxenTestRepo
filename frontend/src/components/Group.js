f50f47c2302f9abc361e52c450aa0e5781b52cae
StyxenStones <joseph70590@gmailcom>
2020-11-22 02:20:28 -0500

    2

--- frontend/src/components/Group.js
+++ frontend/src/components/Group.js
@@ -78,6 +78,42 @@ function Group() {
                     </form>
                 </div>
             </div>
+            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#form">
+                Add New Group
+            </button>
+                <div>
+                <div class="modal fade" id="form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header border-bottom-0">
+                        <h5 class="modal-title" id="exampleModalLabel">Create Account</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div>
+                    <form onSubmit={addGroup}>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="groupname">Group Name</label>
+                                <input type="text" class="form-control" id="group-name" aria-describedby="group-name" placeholder="Enter group name" ref={(c) => addGroupName = c}></input>
+                                <small id="group-name" class="form-text text-muted">Name your group</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="group-description">Group Description</label>
+                                <input type="text" class="form-control" id="group-description" aria-describedby="group-description" placeholder="Enter group description" ref={(c) => addGroupDescription = c}></input>
+                                <small id="group-description" class="form-text text-muted">Describe your group</small>
+                            </div>
+                        </div>
+                        <div class="modal-footer border-top-0 d-flex justify-content-center">
+                        <button type="submit" class="btn btn-success" onClick={addGroup}>Submit</button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+                </div>
+                </div>
+            </div>
         </div>
    );
 }
