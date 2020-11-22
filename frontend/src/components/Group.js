0a8447160f3ed6faf9c2e16822c2f44175fe9e36
StyxenStones <joseph70590@gmailcom>
2020-11-22 18:08:41 -0500

    2

--- frontend/src/components/Group.js
+++ frontend/src/components/Group.js
@@ -80,8 +80,10 @@ function createMemberList()
     for(i = 0; i < members.length; i++)
     {
         var button = document.createElement("button");
+        button.type = "submit";
+        button.className = "btn btn-danger";
         button.innerHTML = members[i].userID;
-        button.addEventListener("click", openAccount(members[i].userID));
+        button.addEventListener("click", () =>openAccount(members[i].userID));
         div.appendChild(button);
     }
     document.getElementById("memberButtons").appendChild(div);
